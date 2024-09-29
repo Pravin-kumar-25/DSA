0ae666110e36d509867a0d9c48e376a3dcdfc553
Pravin-kumar-25 <pravinkumar3725@gmail.com>
2024-09-29 12:26:08 +0530

    Added circular deque dsa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyCircularDeque obj = new MyCircularDeque(3);
+        obj.insertFront(9);
+        System.out.println(obj.getRear());
+        obj.insertFront(9);
+        System.out.println(obj.getRear());
+        obj.insertLast(5);
+        System.out.println(obj.getFront());
+        System.out.println(obj.getRear());
+        System.out.println(obj.getFront());
+        System.out.println(Arrays.toString(obj.getArray()));
+        obj.insertLast(8);
+        System.out.println(Arrays.toString(obj.getArray()));
+
+        obj.deleteLast();
+        System.out.println(Arrays.toString(obj.getAll()));
+
+        System.out.println(Arrays.toString(obj.getArray()));
+        System.out.println(obj.getFront());
+
+        System.out.println(Arrays.toString(obj.getArray()));
     }
 }
